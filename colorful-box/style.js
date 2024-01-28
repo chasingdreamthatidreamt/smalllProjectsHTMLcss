e90b9e1bf1eb91fac4bff1b47fdb5086bc1668a1
chasingdreamthatidreamt <143831135+chasingdreamthatidreamt@users.noreply.github.com>
2024-01-27 23:47:44 -0700

    finished

--- colorful-box/style.js
+++ colorful-box/style.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     let audio = document.querySelector("audio");
     let boxes = document.querySelectorAll(".box");
+    let playbackPositions = {};
 
     boxes.forEach(function(box) {
-        box.addEventListener("hover", function() {
+        box.addEventListener("mouseover", function() {
+            if (playbackPositions[box.id]) {
+                audio.currentTime = playbackPositions[box.id];
+            }
             audio.play();
+        });
+
+        box.addEventListener("mouseout", function() {
+            playbackPositions[box.id] = audio.currentTime;
+            audio.pause();
         });
     });
 });
